4b3bbcc4e1fad7b8a619235ea272ccfe5b86c91e
Leo P <junk@slact.net>
2013-10-22 16:15:46 -0400

    non-portable binary rebuild script for testing

---
+++ tests/rebuild.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+#assumes PKGBUILDy nginx located at ../../nginx-nhpm
+MY_PATH="`dirname \"$0\"`"
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"
+pushd ../../nginx-nhpm
+makepkg -f
+ln -svf `pwd`/pkg/nginx-nhpm-dev/usr/bin/nginx $MY_PATH/nginx
+popd
