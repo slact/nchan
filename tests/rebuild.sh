c83e0b4362fe315cd3ea113f1503fd25f4577344
Leo P <junk@slact.net>
2013-12-13 07:23:28 -0500

    test rebuild with nopool option

--- tests/rebuild.sh
+++ tests/rebuild.sh
@@ -3,6 +3,10 @@
 MY_PATH="`dirname \"$0\"`"
 MY_PATH="`( cd \"$MY_PATH\" && pwd )`"
 pushd ./nginx-nhpm
-makepkg -f
-ln -svf `pwd`/pkg/nginx-nhpm-dev/usr/bin/nginx $MY_PATH/nginx
+if [[ $1 == "nopool" ]]; then
+    echo "patching to disable pools (useful for valgrind)"
+    NO_POOL=1 makepkg -f
+else
+    makepkg -f
+fi
 popd
