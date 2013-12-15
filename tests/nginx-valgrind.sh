c46afa4a69fe05ca8e89ebb7d5eb257a017345ff
Leo P <junk@slact.net>
2013-12-15 00:33:16 -0500

    better nginx test runner

--- tests/nginx-valgrind.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/zsh
-valgrind --trace-children=yes --track-fds=no --track-origins=yes ./nginx -p ./ -c ./nginx.conf
--- tests/nginx.sh
+++ tests/nginx.sh
@@ -1,2 +1,24 @@
 #!/bin/zsh
-./nginx -p ./ -c ./nginx.conf
+NGINX_OPT=( -p ./ -c ./nginx.conf )
+VALGRIND_OPT=( --trace-children=yes --track-fds=no --track-origins=yes )
+WORKERS=5
+for opt in $*; do
+  case $opt in
+    leak|leakcheck)
+      VALGRIND_OPT+=("--leak-check=full")
+      ;;
+    valgrind)
+      valgrind=1
+      ;;
+    worker|one|single) 
+      WORKERS=1
+      ;;
+  esac
+done
+NGINX_OPT+=( -g "worker_processes $WORKERS;" )
+
+if [[ $valgrind == 1 ]]; then
+  valgrind $VALGRIND_OPT ./nginx $NGINX_OPT
+else
+  ./nginx $NGINX_OPT
+fi
