ed47dfdf74588c88329dc20e71e47fccc2857e05
Leo P <junk@slact.net>
2013-12-23 19:40:33 -0500

    add option to run nginx attached to kdbg

--- tests/nginx.sh
+++ tests/nginx.sh
@@ -2,6 +2,7 @@
 NGINX_OPT=( -p ./ -c ./nginx.conf )
 VALGRIND_OPT=( --trace-children=yes --track-fds=no --track-origins=yes )
 WORKERS=5
+NGINX_DAEMON="off"
 NGINX_CONF="working_directory \"`pwd`\"; "
 for opt in $*; do
   case $opt in
@@ -14,15 +15,29 @@ for opt in $*; do
     worker|1|one|single) 
       WORKERS=1
       ;;
+    debug|kdbg)
+      WORKERS=1
+      NGINX_DAEMON="on"
+      debugger=1
+      ;;
   esac
 done
-NGINX_CONF="worker_processes $WORKERS; $NGINX_CONF"
+NGINX_CONF="worker_processes $WORKERS; daemon $NGINX_DAEMON; $NGINX_CONF"
 NGINX_OPT+=( -g "$NGINX_CONF" )
 #echo $NGINX_CONF
 #echo $NGINX_OPT
+echo "nginx $NGINX_OPT"
 
-if [[ $valgrind == 1 ]]; then
+if [[ $debugger == 1 ]]; then
+  ./nginx $NGINX_OPT
+  sleep 0.2
+  master_pid=`cat /tmp/nhpm-test-nginx.pid`
+  child_pid=`pgrep -P $master_pid`
+  sudo kdbg -p $child_pid ./nginx
+  kill $master_pid
+elif [[ $valgrind == 1 ]]; then
   valgrind $VALGRIND_OPT ./nginx $NGINX_OPT
 else
   ./nginx $NGINX_OPT
 fi
+
