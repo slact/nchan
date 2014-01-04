a3ff6d75f70a8346c0a718ffaf2582f84488f05b
Leo P <junk@slact.net>
2014-01-03 22:54:14 -0500

    nginx launcher got debuglog

--- tests/nginx.sh
+++ tests/nginx.sh
@@ -8,6 +8,7 @@ WORKERS=5
 NGINX_DAEMON="off"
 NGINX_CONF="working_directory \"`pwd`\"; "
 ACCESS_LOG="\\/dev\\/null"
+ERRLOG_LEVEL="notice"
 for opt in $*; do
   if [[ "$opt" = <-> ]]; then
     WORKERS=$opt
@@ -27,6 +28,9 @@ for opt in $*; do
       NGINX_DAEMON="on"
       debugger=1
       ;;
+    debuglog)
+      ERRLOG_LEVEL="debug"
+      ;;
   esac
 done
 NGINX_CONF="worker_processes $WORKERS; daemon $NGINX_DAEMON; $NGINX_CONF"
@@ -38,7 +42,7 @@ echo "nginx $NGINX_OPT"
 #set access log
 echo $ACCESS_LOG
 sed "s|\(access_log\).*|\1 $ACCESS_LOG;|g" ./nginx.conf -i
-
+sed "s|\(^\s*error_log\s\+\S\+\).*|\1 $ERRLOG_LEVEL;|g" ./nginx.conf -i
 if [[ $debugger == 1 ]]; then
   ./nginx $NGINX_OPT
   sleep 0.2
@@ -53,4 +57,3 @@ elif [[ $valgrind == 1 ]]; then
 else
   ./nginx $NGINX_OPT
 fi
-
