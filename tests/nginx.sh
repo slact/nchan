3769e5927988d5257e6fd261f23cc1dbed5e4543
Leo P <junk@slact.net>
2014-01-12 20:22:59 -0500

    use copy cof nginx config for testing

--- tests/nginx.sh
+++ tests/nginx.sh
@@ -1,8 +1,10 @@
 #!/bin/zsh
-
+NGINX_CONFIG=`pwd`/nginx.conf
+NGINX_TEMP_CONFIG=`pwd`/.nginx.thisrun.conf
 NGINX_OPT=( -p `pwd`/ 
-    -c `pwd`/nginx.conf 
+    -c $NGINX_TEMP_CONFIG
 )
+cp -fv $NGINX_CONFIG $NGINX_TEMP_CONFIG
 VALGRIND_OPT=( --trace-children=yes --track-fds=no --track-origins=yes --read-var-info=yes )
 WORKERS=5
 NGINX_DAEMON="off"
@@ -38,11 +40,8 @@ NGINX_OPT+=( -g "$NGINX_CONF" )
 #echo $NGINX_CONF
 #echo $NGINX_OPT
 echo "nginx $NGINX_OPT"
-
-#set access log
-echo $ACCESS_LOG
-sed "s|\(access_log\).*|\1 $ACCESS_LOG;|g" ./nginx.conf -i
-sed "s|\(^\s*error_log\s\+\S\+\).*|\1 $ERRLOG_LEVEL;|g" ./nginx.conf -i
+sed "s|\(access_log\).*|\1 $ACCESS_LOG;|g" $NGINX_TEMP_CONFIG -i
+sed "s|\(^\s*error_log\s\+\S\+\).*|\1 $ERRLOG_LEVEL;|g" $NGINX_TEMP_CONFIG -i
 if [[ $debugger == 1 ]]; then
   ./nginx $NGINX_OPT
   sleep 0.2
