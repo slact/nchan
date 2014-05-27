db6ed22d3e35df430d60b13049f4b5a4907b86aa
Leo P <junk@slact.net>
2014-05-27 16:00:31 -0400

    refined perpetual test runners pub.rb and sub.rb

--- tests/pub.rb
+++ tests/pub.rb
@@ -3,21 +3,46 @@ require 'securerandom'
 require_relative 'pubsub.rb'
 require "optparse"
 server= "localhost:8082"
-OptionParser.new do |opts|
-  opts.on("-s", "--server SERVER", "server and port."){|v| server=v}
+msg=false
+loop=false
+
+opt=OptionParser.new do |opts|
+  opts.on("-s", "--server SERVER (#{server})", "server and port."){|v| server=v}
   opts.on("-v", "--verbose", "Blabberhttp"){Typhoeus::Config.verbose=true}
-end.parse!
+  opts.on("-l", "--loop", "don't wait to send message"){loop=true}
+  opts.on("-m", "--message MSG", "publish this message instead of prompting"){|v| msg=v}
+end
+opt.banner="Usage: pub.rb [options] url"
+opt.parse!
 
 url = "http://#{server}#{ARGV.last}"
 
 puts "Publishing to #{url}."
- 
-a=gets
+
+
+loopmsg=("\r"*20) + "sending message #"
 
 pub = Publisher.new url
-while true do
-  puts "Enter one-line message, press enter."
-  msg=gets #doesn't work when there are parameters. wtf?
-  pub.post msg
-  puts ""
+repeat=true
+i=1
+while repeat do
+  if msg
+    if loop
+      print loopmsg
+      print i
+      i+=1
+    else
+      puts "Press enter to send message."
+      STDIN.gets
+    end
+    pub.post msg
+  else
+    if loop
+      puts "Can't repeat with custom message. use -m option"
+    end
+    puts "Enter one-line message, press enter."
+    message=STDIN.gets #doesn't work when there are parameters. wtf?
+    pub.post message
+    puts ""
+  end
 end
