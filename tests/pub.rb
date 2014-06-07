9c06931e1497c2602f28e053a53c6994bf0b7a5d
Leo P <junk@slact.net>
2014-06-07 18:19:56 -0400

    add repeat interval for pub.rb

--- tests/pub.rb
+++ tests/pub.rb
@@ -5,11 +5,15 @@ require "optparse"
 server= "localhost:8082"
 msg=false
 loop=false
+repeat_sec=0.5
 
 opt=OptionParser.new do |opts|
   opts.on("-s", "--server SERVER (#{server})", "server and port."){|v| server=v}
   opts.on("-v", "--verbose", "Blabberhttp"){Typhoeus::Config.verbose=true}
-  opts.on("-l", "--loop", "don't wait to send message"){loop=true}
+  opts.on("-r", "--repeat [SECONDS]", "re-send message every N seconds (#{repeat_sec})") do |v| 
+    loop=true
+    repeat_sec=Float(v) unless v.nil?
+  end 
   opts.on("-m", "--message MSG", "publish this message instead of prompting"){|v| msg=v}
 end
 opt.banner="Usage: pub.rb [options] url"
@@ -28,6 +32,7 @@ i=1
 while repeat do
   if msg
     if loop
+      sleep repeat_sec
       print loopmsg
       print i
       i+=1
@@ -36,6 +41,7 @@ while repeat do
       STDIN.gets
     end
     pub.post msg
+    
   else
     if loop
       puts "Can't repeat with custom message. use -m option"
