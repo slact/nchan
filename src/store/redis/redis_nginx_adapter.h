567ef7cdf6b0d0fb212eeda7ea5910c5a262a3a9
Leo P <junk@slact.net>
2014-08-23 07:37:28 -0400

    add redis adapter. thanks, Wandenberg Peixoto!

---
+++ src/store/redis/redis_nginx_adapter.h
@@ -0,0 +1,14 @@
+//Copyright (c) 2014 Wandenberg Peixoto under the MIT Licence
+#ifndef __REDIS_NGINX_ADAPTER_H
+#define __REDIS_NGINX_ADAPTER_H
+
+#include <hiredis/hiredis.h>
+#include <hiredis/async.h>
+
+void redis_nginx_init(void);
+redisAsyncContext *redis_nginx_open_context(const char *host, int port, int database, redisAsyncContext **context);
+void redis_nginx_force_close_context(redisAsyncContext **context);
+void redis_nginx_close_context(redisAsyncContext **context);
+void redis_nginx_ping_callback(redisAsyncContext *ac, void *rep, void *privdata);
+
+#endif // __REDIS_NGINX_ADAPTER_H
