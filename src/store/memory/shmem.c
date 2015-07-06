3f088c1d41126a78eaad1c3bc069f0932c0ba5b2
Leo P <junk@slact.net>
2015-07-05 20:14:05 -0400

    oh my that's an oversight. shmem wasn't even locked when allocating!

--- src/store/memory/shmem.c
+++ src/store/memory/shmem.c
@@ -48,7 +48,7 @@ ngx_int_t shm_destroy(shmem_t *shm) {
 }
 void *shm_alloc(shmem_t *shm, size_t size, const char *label) {
   void         *p;
-  if((p = ngx_slab_alloc_locked(SHPOOL(shm), size))==NULL) {
+  if((p = ngx_slab_alloc(SHPOOL(shm), size))==NULL) {
     ngx_log_error(NGX_LOG_ERR, ngx_cycle->log, 0, "shpool alloc failed");
   }
   #if (DEBUG_SHM_ALLOC == 1)
@@ -67,7 +67,7 @@ void *shm_calloc(shmem_t *shm, size_t size, const char *label) {
   return p;
 }
 void shm_free(shmem_t *shm, void *p) {
-  ngx_slab_free_locked(SHPOOL(shm), p);
+  ngx_slab_free(SHPOOL(shm), p);
   #if (DEBUG_SHM_ALLOC == 1)
   ngx_log_error(NGX_LOG_WARN, ngx_cycle->log, 0, "shpool free addr %p", p);
   #endif
