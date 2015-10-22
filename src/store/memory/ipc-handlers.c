808c141dddd0aa02ab5a7ffaa06569acb7413425
Leo P <junk@slact.net>
2015-10-22 06:28:35 -0400

    that assert doesn't belong there (that chanhead pointer is another process's)

--- src/store/memory/ipc-handlers.c
+++ src/store/memory/ipc-handlers.c
@@ -534,7 +534,6 @@ typedef struct {
 ngx_int_t memstore_ipc_send_memstore_subscriber_keepalive(ngx_int_t dst, ngx_str_t *chid, subscriber_t *sub, nchan_store_channel_head_t *ch, callback_pt callback, void *privdata) {
   sub_keepalive_data_t        data = {str_shm_copy(chid), sub, ch, 0, callback, privdata};
   DBG("send SUBBSCRIBER KEEPALIVE to %i %V", dst, chid);
-  assert(ch->status == READY);
   ipc_alert(nchan_memstore_get_ipc(), dst, IPC_SUBSCRIBER_KEEPALIVE, &data, sizeof(data));
   return NGX_OK;
 }
