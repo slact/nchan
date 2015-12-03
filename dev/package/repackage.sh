9461c5a4907808bacd7bf67c51e0cb2b928a244c
Leo P <junk@slact.net>
2015-12-03 13:22:56 -0500

    tar.gz tarball please

--- dev/package/repackage.sh
+++ dev/package/repackage.sh
@@ -12,7 +12,14 @@ popd
 pushd $pkgdir
 archpkg=`echo *pkg.tar.xz`
 
-bundle exec fpm -s pacman -t tar $archpkg
+tarname=${archpkg:r}
+tarname=${tarname:r}
+tarname=${tarname:r}
+
+echo $tarname
+
+bundle exec fpm -s pacman -t tar -n "$tarname" $archpkg
+gzip -f "$tarname.tar"
 
 bundle exec fpm -s pacman --no-auto-depends -t deb $archpkg
 #bundle exec fpm -s pacman -t rpm $archpkg
