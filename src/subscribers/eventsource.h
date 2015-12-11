19c5fc9bd6643ec0c0314f69ee2d51150fe8ea65
Leo P <junk@slact.net>
2015-12-11 04:57:32 -0500

    don't need eventsource_subscriber_destroy

--- src/subscribers/eventsource.h
+++ src/subscribers/eventsource.h
@@ -1,4 +1,4 @@
 subscriber_t *eventsource_subscriber_create(ngx_http_request_t *r, nchan_msg_id_t *msg_id);
-ngx_int_t eventsource_subscriber_destroy(subscriber_t *sub);
+//ngx_int_t eventsource_subscriber_destroy(subscriber_t *sub);
 
 ngx_int_t nchan_detect_eventsource_request(ngx_http_request_t *r);
