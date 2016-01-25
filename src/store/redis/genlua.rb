a0f253b04cbd43183cd64af52ab43a8f9c3cd6bc
Leo P <junk@slact.net>
2016-01-25 03:02:37 -0500

    check redis script syntax before generating c strings

--- src/store/redis/genlua.rb
+++ src/store/redis/genlua.rb
@@ -5,6 +5,8 @@ scripts={}
 
 Dir[ "#{File.dirname(__FILE__)}/scripts/*.lua" ].each do |f|
   scripts[File.basename(f, ".lua") .to_sym]=IO.read f
+  
+  exit 1 unless system "luac5.1 -p #{f}"
 end
 
 def cquote(str)
