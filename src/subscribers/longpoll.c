9fd691ff7bfdc343de2cb64773c7c635a936088f
Leo P <junk@slact.net>
2016-01-28 11:54:50 -0500

    fix longpoll-multipart abort handling

--- src/subscribers/longpoll.c
+++ src/subscribers/longpoll.c
@@ -257,13 +257,13 @@ static ngx_int_t longpoll_respond_message(subscriber_t *self, nchan_msg_t *msg) 
   ctx->prev_msg_id = self->last_msgid;
   update_subscriber_last_msg_id(self, msg);
   ctx->msg_id = self->last_msgid;
-  
-  //disable abort handler
-  fsub->data.cln->handler = empty_handler;
 
   //verify_unique_response(&fsub->data.request->uri, &self->last_msgid, msg, self);
   
   if(!cf->longpoll_multimsg) {
+    //disable abort handler
+    fsub->data.cln->handler = empty_handler;
+    
     assert(fsub->data.already_responded != 1);
     fsub->data.already_responded = 1;
     if(ctx->request_origin_header.len > 0) {
@@ -298,6 +298,9 @@ static ngx_int_t longpoll_multimsg_respond(full_subscriber_t *fsub) {
   ngx_buf_t              double_newline_buf;
   ngx_str_t             *content_type;
   size_t                 size = 0;
+  
+  //disable abort handler
+  fsub->data.cln->handler = empty_handler;
   
   nchan_longpoll_multimsg_t *first, *cur;
   
