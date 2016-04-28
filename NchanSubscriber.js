a076cd3f7e96026f859d8b5e82fc9fba848d0db9
Leo P <junk@slact.net>
2016-04-28 18:07:16 -0400

    comment NchanSubscriber.js a bit

--- NchanSubscriber.js
+++ NchanSubscriber.js
@@ -1,3 +1,40 @@
+/*
+ * NchanSubscriber
+ * usage: var sub = NchanSubscriber(url, opt);
+ * 
+ * opt = {
+ *   subscriber: 'longpoll', 'eventsource', or 'websocket'
+ *     //or an array of the above indicating subscriber type preference
+ * }
+ * 
+ * sub.on("message", function(message, message_metadata) {
+ *   // message is a string
+ *   // message_metadata may contain 'id' and 'content-type'
+ * });
+ * 
+ * sub.on('connect', function(evt) {
+ *   //fired when first connected. 
+ * }
+ * 
+ * sub.on('disconnect', function(evt) {
+ *   // when disconnected.
+ * }
+ * 
+ * sub.on('error', function(code, message) {
+ *   //error callback. not sure about the parameters yet
+ * }
+ * 
+ * sub.reconnect; // should subscriber try to reconnect? true by default.
+ * sub.reconnectTimeout; //how long to wait to reconnect? does not apply to EventSource
+ * 
+ * sub.start(); // begin (or resume) subscribing
+ * sub.stop(); // stop subscriber. do not reconnect.
+ */
+
+
+
+
+
 
 // https://github.com/yanatan16/nanoajax
 !function(t,e){function n(t){return t&&e.XDomainRequest&&!/MSIE 1/.test(navigator.userAgent)?new XDomainRequest:e.XMLHttpRequest?new XMLHttpRequest:void 0}function o(t,e,n){t[e]=t[e]||n}var r=["responseType","withCredentials","timeout","onprogress"];t.ajax=function(t,a){function s(t,e){return function(){c||(a(void 0===f.status?t:f.status,0===f.status?"Error":f.response||f.responseText||e,f),c=!0)}}var u=t.headers||{},i=t.body,d=t.method||(i?"POST":"GET"),c=!1,f=n(t.cors);f.open(d,t.url,!0);var l=f.onload=s(200);f.onreadystatechange=function(){4===f.readyState&&l()},f.onerror=s(null,"Error"),f.ontimeout=s(null,"Timeout"),f.onabort=s(null,"Abort"),i&&(o(u,"X-Requested-With","XMLHttpRequest"),e.FormData&&i instanceof e.FormData||o(u,"Content-Type","application/x-www-form-urlencoded"));for(var p,m=0,v=r.length;v>m;m++)p=r[m],void 0!==t[p]&&(f[p]=t[p]);for(var p in u)f.setRequestHeader(p,u[p]);return f.send(i),f},e.nanoajax=t}({},function(){return this}());
