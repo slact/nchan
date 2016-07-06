d2a0f03434c73c382e13d41bff6239c61c53e221
Leo P <junk@slact.net>
2016-07-06 17:58:38 -0400

    memstore_channel_owner should be a publicly visible function... for debugging probably

--- src/store/memory/store.h
+++ src/store/memory/store.h
@@ -6,4 +6,5 @@ extern nchan_store_t  nchan_store_memory;
 ngx_int_t msg_reserve(nchan_msg_t *msg, char *lbl);
 ngx_int_t msg_release(nchan_msg_t *msg, char *lbl);
 
+ngx_int_t memstore_channel_owner(ngx_str_t *id);
 #endif //NCHAN_MEMSTORE_H
