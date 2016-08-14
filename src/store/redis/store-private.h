266742ea1c83d44506aa28fd284b2d27263331bb
Leo P <junk@slact.net>
2016-08-14 00:27:00 -0400

    some stuff...

--- src/store/redis/store-private.h
+++ src/store/redis/store-private.h
@@ -183,6 +183,8 @@ ngx_int_t redis_chanhead_gc_withdraw(rdstore_channel_head_t *head);
 ngx_int_t redis_chanhead_gc_withdraw_from_reaper(nchan_reaper_t *, rdstore_channel_head_t *head);
 
 void redis_associate_chanhead_with_rdata(rdstore_channel_head_t *head, rdstore_data_t *rdata);
+nchan_reaper_t *rdstore_get_chanhead_reaper(rdstore_channel_head_t *ch);
+
 rdstore_data_t *find_rdata_by_connect_params(redis_connect_params_t *rcp);
 rdstore_data_t *find_rdata_by_url(ngx_str_t *url);
 #endif //NCHAN_REDIS_STORE_PRIVATE_H
