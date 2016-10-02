339a231b72532e20a4249fe07a10434480cbd2f1
Leo P <junk@slact.net>
2016-10-02 03:24:53 -0400

    nicer authserver verbose output

--- dev/authserver.rb
+++ dev/authserver.rb
@@ -7,8 +7,20 @@ require "reel/rack/cli"
 require 'reel/rack'
 require "optparse"
 
+def print_request(env)
+  out = []
+  out << "  #{env["REQUEST_METHOD"]} #{env["PATH_INFO"]}"
+  out << "  Host: #{env["HTTP_HOST"]}"
+  env.each do |k,v|
+    if k != "  HTTP_HOST" && k =~ /^HTTP_/
+      out << "  #{k.split("_").slice(1..-1).each(&:capitalize!).join('-')}: #{v}"
+    end
+  end
+  puts out.join("\n")
+end
+
 class AuthServer
-  if __FILE__ != $PROGRAM_NAME then
+  if __FILE__ != $PROGRAM_NAME
     include Celluloid::IO
   end
   attr_accessor :app
@@ -22,7 +34,7 @@ class AuthServer
       code = 200
       body = env["rack.input"].read
       
-      pp env if @opt[:verbose]
+      print_request env if @opt[:verbose]
       
       case env["REQUEST_PATH"] || env["PATH_INFO"]
       when "/accel_redirect"  
