41616bbb96fba5471dcf773498c714d6024f1efd
Leo P <junk@slact.net>
2016-12-02 01:39:01 -0500

    more descriptive ipc function headers

--- src/util/nchan_rbtree.h
+++ src/util/nchan_rbtree.h
@@ -35,18 +35,18 @@ struct ngx_rbtree_debug_node_s {
 
 typedef enum {RBTREE_WALK_LEFT, RBTREE_WALK_RIGHT, RBTREE_WALK_LEFT_RIGHT, RBTREE_WALK_STOP} rbtree_walk_direction_t;
 
-typedef ngx_int_t (*rbtree_walk_callback_pt)(rbtree_seed_t *, void *, void *);
+typedef ngx_int_t (*rbtree_walk_callback_pt)(rbtree_seed_t *, void *node_data, void *privdata);
 typedef rbtree_walk_direction_t (*rbtree_walk_conditional_callback_pt)(rbtree_seed_t *, void *, void *);
 
 ngx_int_t rbtree_init(rbtree_seed_t *seed, char *name, void *(*id)(void *), uint32_t (*hash)(void *), ngx_int_t (*compare)(void *, void *));
 unsigned             rbtree_empty(rbtree_seed_t *, rbtree_walk_callback_pt, void *data);
 
-ngx_rbtree_node_t   *rbtree_create_node(rbtree_seed_t *, size_t);
+ngx_rbtree_node_t   *rbtree_create_node(rbtree_seed_t *, size_t data_size);
 ngx_int_t            rbtree_insert_node(rbtree_seed_t *, ngx_rbtree_node_t *);
 ngx_int_t            rbtree_remove_node(rbtree_seed_t *, ngx_rbtree_node_t *);
 ngx_int_t            rbtree_destroy_node(rbtree_seed_t *, ngx_rbtree_node_t *);
 
-ngx_rbtree_node_t   *rbtree_find_node(rbtree_seed_t *, void *);
+ngx_rbtree_node_t   *rbtree_find_node(rbtree_seed_t *, void *id);
 
 ngx_int_t            rbtree_walk(rbtree_seed_t *seed, rbtree_walk_callback_pt, void *data);
 ngx_int_t            rbtree_walk_incr(rbtree_seed_t *seed, rbtree_walk_callback_pt callback, void *data);
