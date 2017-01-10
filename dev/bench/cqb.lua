d3ab62d1d92ff43bb1e7d4949af48716fa089402
Leo P <junk@slact.net>
2017-01-10 15:45:40 -0500

    process & thread communication for exiting cleanly

--- dev/bench/cqb.lua
+++ dev/bench/cqb.lua
@@ -30,23 +30,37 @@ if not opt.slave and not opt.master then
   opt.slave = true
 end 
 
+local slave_threads = setmetatable({}, {__mode='k'})
+
 if opt.slave then
   local opt_json = Json.encode(opt)
   for i=1,tonumber(opt.threads) do
-    cq:newThread(function(con, threadnum, opt_json)
+    local thread, con = cq:newThread(function(con, threadnum, opt_json)
       local cqueues = require "cqueues"
       local Json = require "cjson"
       local ut = require("bench.util")
       local cq = cqueues.new()
       ut.accessorize(cq)
       local opt = Json.decode(opt_json)
-      local slave = require "bench.slave"
+      local Slave = require "bench.slave"
+      local slave
       cq:wrap(function()
-        slave(cq, opt)
+        slave = Slave(cq, opt)
         print("started slave thread " .. threadnum)
+      end)
+      cq:wrap(function()
+        for ln in con:lines() do
+          if ln == "exit" then
+            cq:timeout(threadnum, function()
+              print("quitty from thread "  .. threadnum) --quit stuff
+              con:write("exited\n")
+            end)
+          end
+        end
       end)
       assert(cq:loop())
     end, opt_json)
+    slave_threads[thread] = con
   end
 end
 
@@ -65,7 +79,29 @@ end
 
 cq:handleSignal({"SIGINT", "SIGTERM"}, function()
   print("outta here")
-  os.exit(1)
+  local exiting = 0
+  local exited = 0
+  
+  for thread, con in pairs(slave_threads) do
+    con:write("exit\n")
+    exiting = exiting + 1
+    cq:wrap(function()
+      for ln in con:lines() do
+        if ln == "exited" then
+          exited = exited + 1
+          if exited == exiting then
+            print("we're done here")
+            os.exit(1)
+          end
+        end
+      end
+    end)
+  end
+  
+  if exiting == 0 then
+    print("no slave threads. quit now")
+    os.exit(1)
+  end
 end)
 
 while true do
