be4c53c7f45be0e5d99fcf701c86718c3fdb605b
Leo P <junk@slact.net>
2017-11-05 12:12:09 -0500

    fix: disappearing websocket subscriber invalid memory access when running channel existence check via redis

--- src/subscribers/websocket.c
+++ src/subscribers/websocket.c
@@ -296,8 +296,8 @@ static ngx_int_t websocket_finalize_request(full_subscriber_t *fsub) {
     if(sub->enqueued) {
       sub->fn->dequeue(sub);
     }
+    sub->status = DEAD;
     nchan_http_finalize_request(r, NGX_HTTP_OK);
-    
   }
   return NGX_OK;
 }
