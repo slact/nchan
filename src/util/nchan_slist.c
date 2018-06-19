adab17a03fa0b1fbaf811dbb63ce16a0e8685811
Leo P <junk@slact.net>
2018-06-18 22:18:41 -0400

    utils: non-allocating simple linked list

---
+++ src/util/nchan_slist.c
@@ -0,0 +1,114 @@
+#include "nchan_slist.h"
+
+ /*
+  * Boring non-allocating doubly-linked "static" list.
+  *uses preset offsets for the prev and next fields on pre-allocated data
+  */
+ 
+ngx_int_t __nchan_slist_init(nchan_slist_t *list, off_t offset_prev, off_t offset_next) {
+  list->head = NULL;
+  list->tail = NULL;
+  list->n = 0;
+  list->offset.prev = offset_prev;
+  list->offset.next = offset_next;
+  return NGX_OK;
+}
+
+void *nchan_slist_first(nchan_slist_t *list) {
+  return list->head;
+}
+void *nchan_slist_last(nchan_slist_t *list) {
+  return list->tail;
+}
+static void **nchan_slist_next_ptr(nchan_slist_t *list, void *el) {
+  return (void **)&(((char *)el)[list->offset.next]);
+}
+static void **nchan_slist_prev_ptr(nchan_slist_t *list, void *el) {
+  return (void **)&(((char *)el)[list->offset.prev]);
+}
+
+void *nchan_slist_next(nchan_slist_t *list, void *el) {
+  void **ptr = nchan_slist_next_ptr(list, el);
+  return *ptr;
+}
+void *nchan_slist_prev(nchan_slist_t *list, void *el) {
+  void **ptr = nchan_slist_prev_ptr(list, el);
+  return *ptr;
+}
+
+ngx_int_t nchan_slist_append(nchan_slist_t *list, void *el) {
+  void **el_prev_ptr = nchan_slist_prev_ptr(list, el);
+  void **el_next_ptr = nchan_slist_next_ptr(list, el);
+  if(list->head == NULL) {
+    list->head = el;
+  }
+  if(list->tail == NULL) {
+    list->tail = el;
+    *el_prev_ptr = NULL;
+  }
+  else {
+    void **tail_next = nchan_slist_next_ptr(list, list->tail);
+    *el_prev_ptr = list->tail;
+    *tail_next = el;
+  }
+  *el_next_ptr = NULL;
+  list->n++;
+  return NGX_OK;
+}
+ngx_int_t nchan_slist_prepend(nchan_slist_t *list, void *el) {
+  void **el_prev_ptr = nchan_slist_prev_ptr(list, el);
+  void **el_next_ptr = nchan_slist_next_ptr(list, el);
+  if(list->tail == NULL) {
+    list->tail = el;
+  }
+  if(list->head == NULL) {
+    list->head = el;
+    *el_next_ptr = NULL;
+  }
+  else {
+    void **head_prev_ptr = nchan_slist_next_ptr(list, list->head);
+    *el_next_ptr = list->head;
+    *head_prev_ptr = el;
+  }
+  *el_prev_ptr = NULL;
+  list->n++;
+  return NGX_OK;
+}
+ngx_int_t nchan_slist_remove(nchan_slist_t *list, void *el) {
+  void **el_prev_ptr = nchan_slist_prev_ptr(list, el);
+  void **el_next_ptr = nchan_slist_next_ptr(list, el);
+  void *el_prev = *el_prev_ptr, *el_next = *el_next_ptr;
+  if(list->head == el) {
+    list->head = el_next;
+  }
+  if(list->tail == el) {
+    list->tail = el_prev;
+  }
+  if(el_prev) {
+    void **el_prev_next = nchan_slist_prev_ptr(list, el_prev);
+    *el_prev_next = el_next;
+  }
+  if(el_next) {
+    void **el_next_prev = nchan_slist_next_ptr(list, el_next);
+    *el_next_prev = el_prev;
+  }
+  *el_prev_ptr = NULL;
+  *el_next_ptr = NULL;
+  return NGX_OK;
+}
+
+void *nchan_slist_shift(nchan_slist_t *list) {
+  void *head = list->head;
+  if(head) {
+    nchan_list_remove(list, head);
+  }
+  return head;
+}
+
+void *nchan_slist_pop(nchan_slist_t *list) {
+  void *tail = list->tail;
+  if(tail) {
+    nchan_list_remove(list, tail);
+  }
+  return tail;
+}
