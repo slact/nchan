13a3e5d43fd99d0a2aa903042b500bc9a7b4ba70
Leo P <junk@slact.net>
2018-09-10 11:25:08 -0400

    i guess we can bring the debug.sh script up to speed

--- dev/debug.sh
+++ dev/debug.sh
@@ -1,11 +1,9 @@
 #!/bin/zsh
 TESTDIR=`pwd`
 SRCDIR=$(readlink -m $TESTDIR/../src)
-ln -sf $TESTDIR/nginx $SRCDIR/nginx >/dev/null
-ln -sf $TESTDIR/nginx-pushmodule/src/nginx/src/ $SRCDIR/nginx-source >/dev/null
 if [[ "$1" = <-> ]]; then
     sudo kdbg -p $1 $SRCDIR/nginx
 else
     kdbg $SRCDIR/nginx $1
 fi
-rm $SRCDIR/nginx $SRCDIR/nginx-source
+
