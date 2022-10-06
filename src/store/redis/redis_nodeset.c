9c181687ee68de23cfddf5486e876809c260f96e
Leo P <junk@slact.net>
2022-10-06 17:18:59 -0400

    this doesn't cause a bug only because disconnects clean up the `command_received` stuff anyway. Otherwise this would have broken command timeout detection on cluster checks... again.

--- src/store/redis/redis_nodeset.c
+++ src/store/redis/redis_nodeset.c
@@ -987,13 +987,12 @@ static void nodeset_cluster_check_event_callback(redisAsyncContext *ac, void *re
   const char                 *err = NULL;
   
   node_command_time_finish(node, NCHAN_REDIS_CMD_CLUSTER_CHECK);
+  node_command_received(node);
   
   if(reply == NULL) {
     //Node should have already disconnected
     return;
   }
-  
-  node_command_received(node);
   
   if(reply->type == REDIS_REPLY_ERROR) {
     err = reply->str ? reply->str : "error in reply";
